ce489b2fa7f806c157951433d1d336753bcc6514
Sv-Dobrova <sv-dobrova@yandex.ru>
2024-07-20 21:57:35 +0700

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,4 @@
+course_name = "Python"
+home_work = 12
+hours = 1.5
+print ('Курс:', course_name, "Всего задач: ", home_work, "Затрачено часов: ", hours, "среднее время выполнения: ", hours/home_work, "часа")
